5b1cd6a9d6cf9290f59caf973a0e3ada3a6206a3
tbarthelemy <tanguy.barthelemy@insee.fr>
2025-09-29 08:19:45 +0000

    export to S3 the results

--- R/comp.R
+++ R/comp.R
@@ -14,7 +14,7 @@ library("plotly")
 source("R/utils.R")
 
 nb_tent <- 1000
-data_time <- data.frame(
+this_time <- data.frame(
     nb_series = integer(nb_tent),
     cruncher_v2 = numeric(nb_tent),
     cruncher_v3 = numeric(nb_tent),
@@ -33,7 +33,7 @@ for (tentative in seq_len(nb_tent)) {
 
     nb_series <- sample(2:1000, size = 1L)
     cat("Nb series: ", nb_series, "\n")
-    data_time[tentative, "nb_series"] <- nb_series
+    this_time[tentative, "nb_series"] <- nb_series
 
     data <- create_data(n = nb_series)
     data_ts <- ts(data[, -1], start = 2000, freq = 12)
@@ -75,22 +75,59 @@ for (tentative in seq_len(nb_tent)) {
     cat("Coup de RJDemetra\n")
     time_rjdemetra <- call_RJDemetra(data_ts)
 
-    data_time[tentative, "cruncher_v2"] <- time_cruncher_v2[3]
-    data_time[tentative, "cruncher_v3"] <- time_cruncher_v3[3]
-    data_time[tentative, "RJDemetra"] <- time_rjdemetra[3]
-    data_time[tentative, "rjd3x13"] <- time_rjd3x13[3]
+    this_time[tentative, "cruncher_v2"] <- time_cruncher_v2[3]
+    this_time[tentative, "cruncher_v3"] <- time_cruncher_v3[3]
+    this_time[tentative, "RJDemetra"] <- time_rjdemetra[3]
+    this_time[tentative, "rjd3x13"] <- time_rjd3x13[3]
 }
 
 
-p <- tidyr::pivot_longer(data_time, cols = -nb_series) |>
-    ggplot2::ggplot() +
-    ggplot2::geom_point(aes(x = nb_series, y = value, color = name)) +
-    theme_minimal()
+try({
+    p <- tidyr::pivot_longer(this_time, cols = -nb_series) |>
+        ggplot2::ggplot() +
+        ggplot2::geom_point(aes(x = nb_series, y = value, color = name)) +
+        theme_minimal()
 
-# le rendre interactif
-ggplotly(p) |>
-    layout(legend = list(itemclick = "toggleothers"))
+    # le rendre interactif
+    ggplotly(p) |>
+        layout(legend = list(itemclick = "toggleothers"))
+})
 
+# Enregistrement
 
-TBox::write_data(data_time, "output/data_time.csv")
-save.image("output/data.RData")
+library(aws.s3)
+
+BUCKET <- "tbarthelemy"
+FILE_KEY_S3 <- "data/time.csv"
+
+time_total <-
+    aws.s3::s3read_using(
+        FUN = TBox::get_data,
+        object = FILE_KEY_S3,
+        bucket = BUCKET,
+        opts = list("region" = "")
+    )
+
+BUCKET_OUT = "tbarthelemy"
+FILE_KEY_OUT_S3 = "data/time.csv"
+
+new_time_total <- rbind(time_total, this_time)
+
+aws.s3::s3write_using(
+    new_time_total,
+    FUN = TBox::write_data,
+    object = FILE_KEY_OUT_S3,
+    bucket = BUCKET_OUT,
+    opts = list("region" = "")
+)
+
+BUCKET_OUT = "tbarthelemy"
+FILE_KEY_OUT_S3 = paste0("data/time-", Sys.time(), ".csv")
+
+aws.s3::s3write_using(
+    this_time,
+    FUN = TBox::write_data,
+    object = FILE_KEY_OUT_S3,
+    bucket = BUCKET_OUT,
+    opts = list("region" = "")
+)
